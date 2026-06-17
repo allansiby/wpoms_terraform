85a61baff86f469cce7d1c783dd4bfa3de6798d6
Alan Siby <imalansiby@gmail.com>
2026-06-17 21:50:23 +0530

    added security groups

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,6 +70,14 @@ resource "aws_security_group" "alan_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+  description = "PostgreSQL"
+  from_port   = 5432
+  to_port     = 5432
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
